b5eb89a4932f00ea1a6b96b0f4921dedd80b2341
Samuel Hinkhouse <shinkhouse@nfsmith.com>
2022-01-13 17:01:49 -0600

    ui changes

--- src/app/folder/folder.module.ts
+++ src/app/folder/folder.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { FolderPageRoutingModule } from './folder-routing.module';
 
 import { FolderPage } from './folder.page';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    FolderPageRoutingModule
+    FolderPageRoutingModule,
+    FlexLayoutModule
   ],
   declarations: [FolderPage]
 })
